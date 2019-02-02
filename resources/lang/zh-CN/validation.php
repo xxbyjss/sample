bd9350b728ca5f30d7b0c2c56b30b49433fbb1f2
许晓彬 <2718283654@qq.com>
2019-02-02 16:45:42 +0800

    Finish user signup

---
+++ resources/lang/zh-CN/validation.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Custom Validation Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | Here you may specify custom validation messages for attributes using the
+    | convention "attribute.rule" to name the lines. This makes it quick to
+    | specify a specific custom language line for a given attribute rule.
+    |
+    */
+
+    'custom' => [
+        'email' => [
+            'required' => '邮箱 不能为空！！',
+        ],
+    ],
+
+
+];
